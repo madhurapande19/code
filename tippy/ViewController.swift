97d740b2805d67af4432e0cd4081f527650ebd06
Madhura <madhura_pande@intuit.com>
2017-03-11 19:57:33 +0530

    first commit

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercent = [0.18,0.2,0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill*tipPercent[tipControl.selectedSegmentIndex]
+        let total = bill+tip
+        tipLabel.text = String(format: "$%0.2f",tip)
+        totalLabel.text = String(format: "$%0.2f",total)
+    }
 }
 
